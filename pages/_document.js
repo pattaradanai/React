36a2b517ed4773790774f16d783d57f1ef8d07dd
pattaradanai <billlove.sick@hotmail.com>
2018-04-10 15:09:26 +0700

    ddd
    sssd

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
+          <script src="https://unpkg.com/react-image/umd/index.min.js" />
           <title>Satit</title>
         </Head>
         <body>
--- pages/home.js
+++ pages/home.js
@@ -5,8 +5,8 @@ import { compose, withState, withHandlers } from 'recompose'
 import { Field, reduxForm } from 'redux-form'
 import Link from 'next/link'
 import css from '../style.css'
-
 import withApp from '../components/withApp'
+
 const Home = () => (
   <div className={css.wrapper}>
     <section className={css.boxes}>
@@ -23,15 +23,15 @@ const Home = () => (
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
       </div>
       <div className={css.box}>
-        <h3>Support</h3>
+        <h3>Analytics</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
       </div>
       <div className={css.box}>
-        <h3>Support</h3>
+        <h3>Marketing</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
       </div>
       <div className={css.box}>
-        <h3>Support</h3>
+        <h3>Development</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, expedita?</p>
       </div>
     </section>
