269751525fa94bb2ea1032dc34b09c37ca8c76be
pattaradanai <billlove.sick@hotmail.com>
2018-04-10 21:08:53 +0700

    img

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import { connect } from 'react-redux'
 import { compose } from 'recompose'
+import Rotation from 'react-rotation'
 
 import withApp from '../components/withApp'
 import css from '../style.css'
 
-export default () => <div className={css.example}>Welcome React App with Next.js</div>
+export default () => (
+  <div className={css.example}>
+    Welcome React App with Next.js
+    <Rotation>
+      <img src="../images/001.jpeg" />
+      <img src="../images/002.jpeg" />
+      <img src="../images/003.jpeg" />
+      <img src="../images/004.jpeg" />
+    </Rotation>
+  </div>
+)
